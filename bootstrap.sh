e0b570266d59f01578ae540de69fcebe0c3b2b5a
Grant Ramsay <seapagan@gmail.com>
2017-09-23 16:29:59 +0100

    Remove git PPA, not valid for ARM
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,8 +5,7 @@
 THISPATH="$(dirname $(readlink -f "$0"))"
 echo "We are running from : $THISPATH"
 
-# ensure we have the latest packages, including git and sublime text repos
-sudo add-apt-repository ppa:git-core/ppa -y
+# ensure we have the latest packages, including sublime text repos
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt update
