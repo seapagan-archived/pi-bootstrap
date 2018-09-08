e4b8e1f28b35613cf4aa69b8bcada15680e95b38
Seapagan <seapagan@users.noreply.github.com>
2018-09-08 15:27:11 +0100

    Update bootstrap.sh
    delete correct versions of rdoc and ri during ruby update

--- bootstrap.sh
+++ bootstrap.sh
@@ -79,8 +79,8 @@ rbenv global 2.5.1
 
 # we need to erase 2 files temporarily (they will be regenerated) otherwise the installation will pause for overwrite confirmation
 # These are the 'ri' and 'rdoc' scripts
-rm ~/.rbenv/versions/2.4.2/bin/rdoc
-rm ~/.rbenv/versions/2.4.2/bin/ri
+rm ~/.rbenv/versions/2.5.1/bin/rdoc
+rm ~/.rbenv/versions/2.5.1/bin/ri
 # now update RubyGems and the default gems
 gem update --system
 gem update
