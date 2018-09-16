e541d69f931557c5c6c485f4c9b9c5e3ff7b31ad
Seapagan <seapagan@users.noreply.github.com>
2018-09-16 19:31:11 +0100

    fix typo
    Choose correct version of Git to download

--- bootstrap.sh
+++ bootstrap.sh
@@ -29,7 +29,7 @@ fi
 
 # install Git from source since the version in PI repo is usually behind ...
 cd $HOME
-wget https://www.kernel.org/pub/software/scm/git/git-2.18.0.tar.xz
+wget https://www.kernel.org/pub/software/scm/git/git-2.19.0.tar.xz
 tar -xvf git-2.19.0.tar.xz
 cd git-2.19.0
 ./configure --prefix=/usr --with-gitconfig=/etc/gitconfig
